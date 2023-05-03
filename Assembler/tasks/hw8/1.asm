dcce45ffb01ee97af632d03d667840925b95704b
Zem-Alex <zemlyanovaleksei@mail.ru>
2023-05-03 12:19:35 +0500

    add atoi and modify draw line tasks

--- Assembler/tasks/hw8/1.asm
+++ Assembler/tasks/hw8/1.asm
@@ -54,7 +54,58 @@ _drawpixel proc near
     pop bp
     ret
 _drawpixel endp  
+
+_draw_line_x proc near
+	push bp
+    mov bp, sp
+	mov si, word ptr [bp + arg1]
+loop_horisontal:
+    ; Отображение пикселей
     
+    mov bh, 0
+    mov dx, word ptr [bp + arg2]
+    mov cx, word ptr [bp + arg3]
+    mov ax, word ptr [bp + arg4]
+	add cx, si
+    mov ah, 0ch
+    int 10h
+	
+    dec si
+	cmp si,0
+	jne loop_horisontal
+	
+	mov sp, bp
+    pop bp
+    ret
+_draw_line_x endp  
+    
+	
+	
+_draw_line_y proc near
+	push bp
+    mov bp, sp
+	mov si, word ptr [bp + arg1]
+loop_vertical:
+    ; Отображение пикселей
+    
+    mov bh, 0
+    mov dx, word ptr [bp + arg2]
+    mov cx, word ptr [bp + arg3]
+    mov ax, word ptr [bp + arg4]
+	add dx, si
+    mov ah, 0ch
+    int 10h
+	
+    dec si
+	cmp si,0
+	jne loop_vertical
+	
+	mov sp, bp
+    pop bp
+    ret
+_draw_line_y endp  
+	
+	
 start:
     mov ax, data
     mov ds, ax
@@ -67,42 +118,32 @@ start:
     call _setmode
     add sp, 2
     
-	mov di, 0
 	
-loop_horisontal:
-    ; Отображение пикселей
-    mov dx, 0Eh ; color
-    push dx
-    mov dx, di; x
-    push dx
-    mov dx, 25 ; y
-    push dx
-    call _drawpixel
-    add sp, 6
-	
-    inc di
-	cmp di,k
-	jne loop_horisontal
-	
-	mov di, 0
-	
-loop_vertical:
-    ; Отображение пикселей
-    mov dx, di ; color
+	mov dx, 0Eh ; color
     push dx
     mov dx, 25; x
     push dx
-    mov dx, di ; y
+    mov dx, 25 ; y
     push dx
-    call _drawpixel
-    add sp, 6
+	mov dx, k ; Количесвто
+	push dx
+	call _draw_line_x
+	add sp, 8
 	
-    inc di
-	cmp di,k
-	jne loop_vertical
+	mov di, 0
+	
+	mov dx, 0Eh ; color
+    push dx
+    mov dx, 25; x
+    push dx
+    mov dx, 25 ; y
+    push dx
+	mov dx, k ; Количесвто
+	push dx
+	call _draw_line_y
+	add sp, 8
 	
 	
-    
     call _getchar
   
     ; Переходим в текстовый режим 80х25
