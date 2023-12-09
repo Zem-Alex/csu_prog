fffc1ecfbfabd9f7831abcf24da7f50df5d9328f
Zem-Alex <zemlyanovaleksei@mail.ru>
2023-12-09 18:17:38 +0500

    123

--- Graph_Theory/MST_algorithms/MST_algorithms/main.cpp
+++ Graph_Theory/MST_algorithms/MST_algorithms/main.cpp
@@ -5,7 +5,7 @@
 #include "header.hpp" 
 
 int main() {
-    Graph graph("test5.txt");
+    Graph graph("test1.txt");
     int option;
     std::cout << "Choose algorithm (1 - Kruskal, 2 - Prim): ";
     std::cin >> option;
